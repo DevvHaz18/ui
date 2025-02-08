16e4c9fd5a5e26808f7d3318ac130917e0fdf93b
bored <135164672+DevvHaz18@users.noreply.github.com>
2025-02-08 04:58:56 +0000

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1146,7 +1146,7 @@ do
                 --
                 local AxisX = Side == "Left" and SecondBorderOutline.Position.X + 6 or SecondBorderOutline.Position.X + ((SecondBorderOutline.Size.X / 2) - 10) + 12
                 local SectionInline = Utility.AddDrawing("Square", {
-                    Position = Vector2.new(AxisX, (Tab.SectionAxis[Side == "Left" and 1 or 2] == 0 and TabOutline.Position.Y + TabOutline.Size.Y + 20 or 16 + Tab.SectionAxis[Side == "Left" and 1 or 2]))
+                    Position = Vector2.new(AxisX, (Tab.SectionAxis[Side == "Left" and 1 or 2] == 0 and TabOutline.Position.Y + TabOutline.Size.Y + 12 or 12 + Tab.SectionAxis[Side == "Left" and 1 or 2]))
                     Size = Vector2.new((SecondBorderOutline.Size.X / 2) - 8, 24),
                     Thickness = 0,
                     Color = Library.Theme.Inline,
